0a7e296a4f59d4e9a6026f1562733b09d1b8ce76
Hamza Nisar <rhamza126@gmail.com>
2023-10-11 01:25:48 +0100

    Delete bigmoney directory

--- bigmoney/smoker/tesd.py
+++
@@ -1,4 +0,0 @@
-import datetime
-msg="2022-04-04"
-birth_date = datetime.datetime.strptime(msg, '%Y-%m-%d')
-print(birth_date)
--- bigmoney/smoker/test.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('shop.db')
-cursor = conn.execute("SELECT productID from COMPANY")
-jobs = cursor.fetchall()
-print(jobs)
